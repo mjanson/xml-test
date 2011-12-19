84b6ab95f6a572840e7ca04fd13b81ccfcc90cd6
Mike Janson <m@taptera.com>
2011-12-19 13:15:20 -0800

    scalaVersion should not be specified for crossScalaVersions (gotta love the sbt docs!)

--- project/Build.scala
+++ project/Build.scala
@@ -6,7 +6,6 @@ object XMLDiff extends Build {
     name := "xml-diff",
     version := "0.3",
     organization := "com.google",
-    scalaVersion := "2.9.1",
     crossScalaVersions := Seq("2.8.0", "2.8.1", "2.8.2", "2.9.0", "2.9.0-1", "2.9.1"),
     scalaSource in Compile <<= baseDirectory(_ / "src")
   )
